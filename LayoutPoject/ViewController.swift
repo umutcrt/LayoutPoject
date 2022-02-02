6e70adce87be0f0a2f64c37293be520864bdf87c
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-02-02 21:55:17 +0300

    Initial Commit

---
+++ LayoutPoject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutPoject
+//
+//  Created by Umut Cörüt on 2.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
