b4b1bc4b66573cb25d6f299f7512614686d9a728
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-02-05 10:44:58 +0300

    implement login screen

--- LayoutPoject/ViewController.swift
+++ LayoutPoject/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var wrongLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+   
+    @IBAction func buttonTap(_ sender: UIButton) {
+        if userNameTextField.text == "umutcorut",
+        passwordTextField.text == "123456"{
+            wrongLabel.text = "Kullanıcı adı ve parola doğru" }
+        else
+        {wrongLabel.text = "Hatalı kullanıcı adı veya parola! Tekrar dene."}
+    }
+    
 
 }
-
+ 
